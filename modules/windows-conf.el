b844e2bc2dda6660b179c4acc850e390e5888d62
Chris Jones <chris.jones120@outlook.com>
2025-10-07 21:25:54 +1000

    Remove unnecessary if statements

--- modules/windows-conf.el
+++ modules/windows-conf.el
@@ -3,7 +3,6 @@
 ;;; Code:
 (use-package emacs
   :ensure nil
-  :if (eq system-type 'windows-nt)
   :init
   (menu-bar-mode -1)
   (set-language-environment "UTF-8")
@@ -13,14 +12,12 @@
 
 (use-package shell
   :ensure nil
-  :if (eq system-type 'windows-nt)
   :hook (comint-output-filter-functions . comint-strip-ctrl-m)
   :bind ("<f11>" . shell)
   :custom (prefer-coding-system 'utf-8))
 
 (use-package elisp-mode
   :ensure nil
-  :if (eq system-type 'windows-nt)
   :config/el-patch
   ;; byte compilation is wonky on windows and breaks flymake
   (defun elisp-flymake-byte-compile (_report-fn &rest _args) 'ignore))
